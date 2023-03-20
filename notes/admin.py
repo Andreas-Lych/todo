07ea31828b10f2aa40afce473578473ad2d6d0d9
Andreas-Lych <caesar.a.1981@gmail.com>
2023-03-20 21:56:20 +0300

    add new project

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    author = ("name")
+    title = ("title")
+    created_at = ("created_at",)
+    text = ("description")
+
+
+
+from django.contrib import admin
+
 # Register your models here.
